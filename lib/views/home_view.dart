5a3b1ec927cd5ea2370161edfeabbdd9b0d5d048
rabab_hamdyy <rababhamdy38@gmail.com>
2025-07-19 03:43:05 +0300

    two animations

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -8,9 +8,11 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late AnimationController animationController;
-  late Animation<Offset> slideAnimation;
+  late Animation<Offset> slideAnimation1;
+  late Animation<Offset> slideAnimation2;
+
   @override
   void initState() {
     super.initState();
@@ -18,10 +20,16 @@ class _HomeViewState extends State<HomeView>
       duration: const Duration(seconds: 5),
       vsync: this,
     );
-    slideAnimation = Tween<Offset>(
+    slideAnimation1 = Tween<Offset>(
       begin: const Offset(0, -7),
       end: Offset.zero,
     ).animate(animationController);
+
+    slideAnimation2 = Tween<Offset>(
+      begin: const Offset(0, 7),
+      end: Offset.zero,
+    ).animate(animationController);
+
     animationController.forward();
   }
 
@@ -34,9 +42,16 @@ class _HomeViewState extends State<HomeView>
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SlideTransition(position: slideAnimation1,child: Image.asset('assets/news.webp')),
+              ),
               SlideTransition(
-                position: slideAnimation,
-                child: Text('Home View', style: TextStyle(fontSize: 30)),
+                position: slideAnimation2,
+                child: Text(
+                  'World News',
+                  style: TextStyle(fontSize: 30, color: Colors.blueGrey[900]),
+                ),
               ),
             ],
           ),
